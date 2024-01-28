b26d37e1d95b58c044db83676ca5eeca22d4f4de
Erick <esantostaype@gmail.com>
2024-01-28 11:05:06 -0500

    noindex

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -18,9 +18,9 @@ export const Header = () => {
         <header className="site-header">
             <div className={`site-header__content ${ scrollTopData }`}>
                 <a href="/" rel="home" className="logo">
-                    <img src="/images/vortex-iso.svg" width="35" alt="Iso" className="iso" />
-                    <img src="/images/vortex-studio.svg" width="166" alt="Vortex Studio" className="logo__label" />
-                    <img src="/images/vortex-studio-light-bg.svg" width="166" alt="Vortex Studio" className="logo__label__light-bg" />
+                    <img src="/images/vortex-iso.svg" width="35" height="30" alt="Iso" className="iso" />
+                    <img src="/images/vortex-studio.svg" width="166" height="30" alt="Vortex Studio" className="logo__label" />
+                    <img src="/images/vortex-studio-light-bg.svg" width="166" height="30" alt="Vortex Studio" className="logo__label__light-bg" />
                 </a>
                 <button className="nav-button">
                     <div className="nav-button__wrapper">
