04ad49102264b798ca83271f149d8ab589e30929
Erick <esantostaype@gmail.com>
2024-01-28 14:02:46 -0500

    pipipipi

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,7 @@
 export * from './Header';
 export * from './ContactForm';
-export * from './ui/TextField'
+// export * from './ui/TextField'
 // export * from './ui/TypingText'
 export * from './ui/Button'
 export * from './ui/MainButton'
-export * from './services/Process'
+// export * from './services/Process'
